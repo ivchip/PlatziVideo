b175ef365e0e467e533b6e6e3d36321ba503328c
Iván Chipatecua <ivchip@gmail.com>
2019-10-14 19:05:31 -0500

    Finish course route and redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -14,7 +14,15 @@ const Home = ({ myList, trends, originals }) => {
       {myList.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            {myList.map((item) => <CarouselItem key={item.id} {...item} />)}
+            {myList.map((item) => {
+              return (
+                <CarouselItem
+                  key={item.id}
+                  {...item}
+                  isList
+                />
+              );
+            })}
           </Carousel>
         </Categories>
       )}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import App from './routes/App';
 import reducer from './reducers';
 
@@ -87,7 +87,7 @@ const initialState = {
       'cover': 'http://dummyimage.com/800x600.png/FF7A90/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
       'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
-    }
+    },
   ],
   'originals': [
     {
@@ -171,7 +171,8 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
