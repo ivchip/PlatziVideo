8bd1ab97fa8c8f5db613e4a1be9e13cbf7beeb29
Iván Chipatecua <ivchip@gmail.com>
2019-10-27 22:31:53 -0500

    Cambios para soporte de server side render

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,64 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+dotenv.config();
+
+const isProd = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-  entry: './src/index.js',
+  devtool: isProd ? 'hidden-source-map' : 'cheap-source-map',
+  entry: './src/frontend/index.js',
+  mode: process.env.NODE_ENV,
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: isProd ? path.join(process.cwd(), './src/server/public') : '/',
+    filename: isProd ? 'assets/app-[hash].js' : 'assets/app.js',
     publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    minimizer: isProd ? [
+      new TerserPlugin(),
+    ] : [],
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: isProd ? 'assets/vendor-[hash].js' : 'assets/vendor.js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some(chunks => chunks.name !== 'vendor' && /[\\/]node_modules[\\/]/.test(name));
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            fix: true,
+          },
+        },
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -22,21 +67,27 @@ module.exports = {
         },
       },
       {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-        },
-      },
-      {
         test: /\.(s*)css$/,
         use: [
-          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           'css-loader',
-          'sass-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              data: `
+                @import "${path.resolve(__dirname, 'src/frontend/assets/styles/Vars.scss')}";
+                @import "${path.resolve(__dirname, 'src/frontend/assets/styles/Media.scss')}";
+                @import "${path.resolve(__dirname, 'src/frontend/assets/styles/Base.scss')}";
+              `,
+            },
+          },
         ],
       },
       {
-        test: /\.(png|gif|jpe?g)$/i,
+        test: /\.(png|gif|jpg)$/,
         use: [
           {
             'loader': 'file-loader',
@@ -52,12 +103,21 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer(),
+        ],
+      },
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
+      filename: isProd ? 'assets/app-[hash].css' : 'assets/app.css',
     }),
+    isProd ? new CompressionPlugin({
+      test: /\.js$|\.css$/,
+      filename: '[path].gz',
+    }) : () => {},
+    isProd ? new ManifestPlugin() : () => {},
   ],
 };
